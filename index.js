c02dfe551776b46336329e561542734c97bcfbec
Hussein Chebli <HusseinChebli@MacBook-Pro.local>
2023-06-11 18:54:53 -0700

    created files and wrote main code

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Shapes = require('./lib/shapes');
+
+async function promptForLogoDetails() {
+  const answers = await inquirer.prompt([
+    {
+      name: 'text',
+      message: 'Enter text (up to three characters):',
+      validate: (input) => input.length <= 3,
+    },
+    {
+      name: 'textColor',
+      message: 'Enter text color (keyword or hexadecimal):',
+    },
+    {
+      name: 'shape',
+      type: 'list',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      name: 'shapeColor',
+      message: 'Enter shape color (keyword or hexadecimal):',
+    },
+  ]);
+
+  const logo = Shapes.createLogo(
+    answers.text,
+    answers.textColor,
+    answers.shape,
+    answers.shapeColor
+  );
+
+  fs.writeFileSync('logo.svg', logo);
+  console.log('Generated logo.svg');
+}
+
+promptForLogoDetails();
