60a7342730071a178e78a15cf716399769e0c518
hkchebli <38819813+hkchebli@users.noreply.github.com>
2023-06-12 23:49:23 -0400

    fixed some of code

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Shapes = require('./lib/shapes');
 
-async function promptForLogoDetails() {
+async function LogoPrompt() {
   //prompt user to answer questions to gather input for logo using inquirer
   const answers = await inquirer.prompt([
     {
       name: 'text',
       message: 'Enter text (up to three characters):',
-      validate: (input) => input.length <= 3,
     },
     {
       name: 'textColor',
@@ -37,4 +36,4 @@ async function promptForLogoDetails() {
   console.log('Generated logo.svg');
 }
 
-promptForLogoDetails();
+LogoPrompt();
